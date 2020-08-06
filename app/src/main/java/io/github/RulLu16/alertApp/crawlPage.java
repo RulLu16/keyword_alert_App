7f406fd4e1b9cce51a6da34f690f99e478d5bc36
RulLu16 <may05200@naver.com>
2020-08-06 16:34:22 +0900

    로그인 수정. 크롤링 작동 완료

--- app/src/main/java/io/github/RulLu16/alertApp/crawlPage.java
+++ app/src/main/java/io/github/RulLu16/alertApp/crawlPage.java
@@ -1,6 +1,11 @@
 package io.github.RulLu16.alertApp;
 
-import android.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -8,57 +13,72 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class crawlPage {
-    String ID;
-    String password;
-    String keyword;
+import java.io.IOException;
 
-    public crawlPage() {
+public class crawlPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crawl_page);
+        executeCrawl startCrawl=new executeCrawl();
+        startCrawl.execute();
     }
 
-    public void setID(String Id){
-        this.ID=Id;
-    }
+    private class executeCrawl extends AsyncTask<Void, Void, Void>{
+        String str="아직 글이 없어요!";
 
-    public void setPassword(String password){
-        this.password=password;
-    }
-
-    public void setKeyword(String keyword){
-        this.keyword=keyword;
-    }
-
-    public String crawl() throws Exception{
-        String url="http://www.ssodam.com/";
-
-        System.out.println(ID+" "+password+" "+keyword);
-        Connection.Response initial= Jsoup.connect(url)
-                .method(Connection.Method.GET)
-                .execute();
-        Document key=initial.parse();
-        String csrf=initial.cookies().get("csrftoken");
-        //System.out.println(csrf);
-
-        Connection.Response login=Jsoup.connect("http://www.ssodam.com/auth")
-                .cookies(initial.cookies())
-                .data("id",ID, "password", password,"auto","false","csrfmiddlewaretoken",csrf)
-                .method(Connection.Method.POST)
-                .timeout(5000)
-                .execute();
-
-        Document doc=Jsoup.connect("http://www.ssodam.com/board/11/1")
-                .cookies(login.cookies())
-                .timeout(3000000).get();
-        //System.out.println(doc);
-
-        Elements element = doc.select("div.post-element.table-post.visible-xs");
-
-        for(Element el : element.select("td.title.title-align")) {
-            if(el.text().contains(keyword)==true){
-                return "새로운 글이 올라왔어요!";
-            }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
-        return "아직 새로운 글이 없어요!";
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            Intent intent2=getIntent();
+            String id=intent2.getStringExtra("input id");
+            String password=intent2.getStringExtra("input password");
+            String keyword=intent2.getStringExtra("input keyword");
+
+            try{
+                String url="http://www.ssodam.com/";
+
+                //System.out.println(ID+" "+password+" "+keyword);
+                Connection.Response initial= Jsoup.connect(url)
+                        .method(Connection.Method.GET)
+                        .execute();
+                Document key=initial.parse();
+                String csrf=initial.cookies().get("csrftoken");
+
+                Connection.Response login=Jsoup.connect("http://www.ssodam.com/auth")
+                        .cookies(initial.cookies())
+                        .data("id",id, "password", password,"auto","false","csrfmiddlewaretoken",csrf)
+                        .method(Connection.Method.POST)
+                        .timeout(5000)
+                        .execute();
+
+                Document doc=Jsoup.connect("http://www.ssodam.com/board/11/1")
+                        .cookies(login.cookies())
+                        .timeout(3000000).get();
+
+                Elements element = doc.select("div.post-element.table-post.visible-xs");
+
+                for(Element el : element.select("td.title.title-align")) {
+                    if(el.text().contains(keyword)==true){
+                        str="새로운 글이 올라왔어요!\n"+el.text();
+                    }
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView result=findViewById(R.id.crawlingResult);
+            result.setText(str);
+        }
     }
 }
--- app/src/main/java/io/github/RulLu16/alertApp/keywordPage.java
+++ app/src/main/java/io/github/RulLu16/alertApp/keywordPage.java
@@ -1,12 +1,23 @@
 package io.github.RulLu16.alertApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class keywordPage extends AppCompatActivity {
 
     @Override
@@ -16,36 +27,21 @@ public class keywordPage extends AppCompatActivity {
     }
 
     public void onClick_Crawl(View v){
-        String message="";
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //AlertDialog.Builder builder = new AlertDialog.Builder(this);
         EditText inputKeyword=findViewById(R.id.loginKeyword);
         String keyword=inputKeyword.getText().toString();
 
         Intent intent1=getIntent();
 
-        String id=intent1.getStringExtra("inputId");
-        String password=intent1.getStringExtra("inputPassword");
+        String id=intent1.getStringExtra("input id");
+        String password=intent1.getStringExtra("input password");
 
-        crawlPage c=new crawlPage();
+        Intent intent2 = new Intent(getApplicationContext(), crawlPage.class);
+        intent2.putExtra("input id", id);
+        intent2.putExtra("input password", password);
+        intent2.putExtra("input keyword", keyword);
 
-        c.setID(id);
-        c.setPassword(password);
-        c.setKeyword(keyword);
-
-
-        builder.setTitle("후..").setMessage(id+" "+password+" "+keyword);
-        AlertDialog alertDialog1 = builder.create();
-        alertDialog1.show();
-
-        try{
-            message=c.crawl();
-            builder.setTitle("알림").setMessage(message);
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-        }catch(Exception e){
-            builder.setTitle("Error!").setMessage("잘못된 로그인 정보이거나 키워드입니다!");
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-        }
+        startActivity(intent2);
     }
+
 }
